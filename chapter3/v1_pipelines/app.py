e5ed5cedd6c9ce7febb1740996c83e7730d23dfe
joscar-rg <40918848+joscar-rg@users.noreply.github.com>
2024-08-14 02:22:55 +0000

    [Ch 3] Pipelines

--- chapter3/v1_pipelines/app.py
+++ chapter3/v1_pipelines/app.py
@@ -9,5 +9,8 @@ words = [
     "occur shop box metal equal mouse some city"
 ]
 
-most = max((len(string.split(" ")) for string in words))
+# most = max((len(string.split(" ")) for string in words))
+ind_words = (string.split(" ") for string in words)
+word_length = (len(lst) for lst in ind_words)
+most = max(word_length)
 print(most)
